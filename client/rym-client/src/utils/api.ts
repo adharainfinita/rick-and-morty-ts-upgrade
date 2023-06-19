6aeef59f6c38bb2e45675a6d212c66e87e712d4f
Adhara Redruello <113715775+adharainfinita@users.noreply.github.com>
2023-06-19 01:28:43 -0300

    Update api.ts

--- client/rym-client/src/utils/api.ts
+++ client/rym-client/src/utils/api.ts
@@ -1,5 +1,5 @@
 
-export const URL_BASE ="http://rick-and-morty-ts-upgrade-production.up.railway.app";
+export const URL_BASE ="https://rick-and-morty-ts-upgrade-production.up.railway.app";
 export const URL_CHARACTER= "http://rick-and-morty-ts-upgrade-production.up.railway.app/character";
 export const URL_LOGIN = 'http://rick-and-morty-ts-upgrade-production.up.railway.appy/login';
 export const URL_FAVORITES = 'http://rick-and-morty-ts-upgrade-production.up.railway.app/fav';
